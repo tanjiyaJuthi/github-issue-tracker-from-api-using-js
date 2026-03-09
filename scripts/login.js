836958becb4bd9552672955187422cdeb6559cf3
tanjiyaJuthi <tanjiya098@gmail.com>
2026-03-09 13:22:19 +0600

    added js funcionality

---
+++ scripts/login.js
@@ -0,0 +1,23 @@
+const loginBtn = document.querySelector('#login-btn');
+
+const userLogin = () => {
+    let loginUserName = document.querySelector('#login-user-name').value.trim();
+    let loginPassword = document.querySelector('#login-password').value.trim();
+
+    const validUsername = "admin";
+    const validPassword = "admin123";
+
+    if (loginUserName === validUsername && loginPassword === validPassword) {
+        localStorage.setItem('isLoggedIn', 'true');
+
+        alert('Login Successful!');
+
+        window.location.replace('/home.html');
+    } else {
+        alert('Login Failed!');
+
+        return;
+    }
+};
+
+loginBtn.addEventListener('click', userLogin);

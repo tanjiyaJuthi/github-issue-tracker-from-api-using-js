0acc9d5fb818a3b537956106623ebfbc207d845c
tanjiyaJuthi <tanjiya098@gmail.com>
2026-03-09 08:11:45 +0600

    refactored displayIssues and displayModal

--- scripts/tracker.js
+++ scripts/tracker.js
@@ -28,71 +28,68 @@ const getDate = (issue) => {
 
 // show modal
 const displayIssueModal = async(issueId) => {
-    const issueModal = document.querySelector('#issue-modal');
+    loadingSpinner(true);
 
     const issueUrl = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${issueId}`;
     const issueRes = await fetch(issueUrl);
-    const issueD = await issueRes.json();
-    const issueData = issueD.data;
-    console.log(issueData);
+    const {data: issueData} = await issueRes.json();
+    // console.log(issueData);
+
+    loadingSpinner(false);
+
+    const issueModal = document.querySelector('#modal-content');
+
+    // remove max width class
+    issueModal
+        .querySelectorAll('.max-w-\\[120px\\]')
+        .forEach(el => {
+            el.classList.remove('max-w-[120px]');
+        });
 
     issueModal.innerHTML = `
-        <dialog id="issueModal" class="modal modal-bottom sm:modal-middle">
-            <div class="modal-box">
-                <div class="issue-detaills space-y-6">
-                    <div class="issue-open-close-container">
-                        <h3 class="font-semibold text-[24px]">${issueData.title}</h3>
-                        <div class="flex items-center gap-1 justify between">
-                            <p class="${issueData.status === 'open' ? 'green-bg' : 'purple-bg'} text-white py-1 px-3 rounded-2xl">${issueData.status === 'open' ?  'Open' : 'Closed'}</p>
-                            <p><p class="gray-dot"></p> Opened by ${getName(issueData.author) ? getName(issueData.author) : 'Not Applicable'}</p>
-                            <p><p class="gray-dot"></p> ${getDate(issueData)}</p>
-                        </div>
-                    </div>
-                
-                    <p class="gray-color">${issueData.description}</p>
-
-                    <div class="bug-help-enhance uppercase space-y-2">
-                        ${displayLabels(issueData.labels)}
-                    </div>
-
-                    <div class="assigne-priority bg-[#F8FAFC] p-4 flex items-center justify-between mt-6">
-                        <div class="assigne">
-                            <p class="text-gray-color">Assignee:</p>
-                            <p>${getName(issueData.assignee) ? getName(issueData.assignee) : 'Not applicable'}</p>
-                        </div>
-                        
-                        <div class="priority">
-                            <p>Priority:</p>
-                            <p class="${getPriorityStyle(issueData.priority)} font-medium text-[12px] text-center max-w-[80px] rounded-2xl py-[6px] uppercase">${issueData.priority}</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="modal-action">
-                    <form method="dialog">
-                        <button class="btn btn-primary">Close</button>
-                    </form>
-                </div>
+        <div class="issue-open-close-container">
+            <h3 class="font-semibold text-[24px]">${issueData.title}</h3>
+            <div class="flex items-center gap-1 justify-between">
+                <p class="${issueData.status === 'open' ? 'green-bg' : 'purple-bg'} text-white py-1 px-3 rounded-2xl">${issueData.status === 'open' ?  'Open' : 'Closed'}</p>
+                <p><p class="gray-dot"></p> Opened by ${getName(issueData.author) ? getName(issueData.author) : 'Not Applicable'}</p>
+                <p><p class="gray-dot"></p> ${getDate(issueData)}</p>
             </div>
-        </dialog>
+        </div>
+                
+        <p class="gray-color">${issueData.description}</p>
+
+        <div class="bug-help-enhance uppercase flex gap-1 items-center">
+            ${displayLabels(issueData.labels, true)}
+        </div>
+
+        <div class="assigne-priority bg-[#F8FAFC] p-4 flex items-center justify-between mt-6">
+            <div class="assigne">
+                <p class="text-gray-color">Assignee:</p>
+                <p>${getName(issueData.assignee) ? getName(issueData.assignee) : 'Not applicable'}</p>
+            </div>
+            
+            <div class="priority">
+                <p>Priority:</p>
+                <p class="${getPriorityStyle(issueData.priority)} font-medium text-[12px] text-center max-w-[80px] rounded-2xl py-[6px] uppercase">${issueData.priority}</p>
+            </div>
+        </div>
     `;
 
-    const issueDialog = document.querySelector('#issueModal');
-    issueDialog.showModal();
+    const issueDialog = document.querySelector('#issue-modal-dialog');
+
+    issueDialog.classList.add('modal-open');
+
+    issueDialog.querySelector('button').addEventListener('click', () => {
+        issueDialog.classList.remove('modal-open');
+    });
 };
 
 // priority styles
-const getPriorityStyle = (priority) => {
-    const data = {
+const getPriorityStyle = (priority) => ({
         low: 'gray-bg',
         medium: 'yellow-bg',
         high: 'red-bg'
-    };
-
-    const pData = data[priority.toLowerCase()];
-
-    return pData;
-};
+    }[priority.toLowerCase()]);
 
 // show active class in button
 const activeButton = (trackerBtn) => {
@@ -145,49 +142,43 @@ const updateIssueCount = (status) => {
 };
 
 // display labels
-const displayLabels = (labels) => {
+const displayLabels = (labels, isModal = false) => {
     const labelStyles = {
-        'bug': {
-            classes: 'red-bg border border-[#EF4444] rounded-2xl md:py-1 md:max-w-[100px]',
-            icon: '<i class="fa-solid fa-bug"></i>'
-        },
-        'help wanted': {
-            classes: 'yellow-bg border border-[#D97706] rounded-2xl md:py-1 md:max-w-[100px]',
-            icon: '<i class="fa-regular fa-life-ring"></i>'
-        },
-        'enhancement' : {
-            classes: 'green-bg border border-[#00A96E] rounded-2xl md:py-1 md:max-w-[100px]',
-            icon: '<i class="fa-regular fa-lightbulb"></i>'
-        },
-        'documentation' : {
-            classes: 'yellow-bg border border-[#D97706] rounded-2xl md:py-1 md:max-w-[100px]',
-            icon: '<i class="fa-regular fa-clipboard"></i>'
-        },
-        'good first issue' : {
-            classes: 'gray-bg border border-[#9CA3AF] rounded-2xl md:py-1 md:max-w-[100px]',
-            icon: '<i class="fa-brands fa-goodreads"></i>'
-        }
+        bug:
+            ['red-bg border-[#EF4444]', 'fa-bug'],
+        'help wanted':
+            ['yellow-bg border-[#D97706]', 'fa-life-ring'],
+        enhancement:
+            ['green-bg border-[#00A96E]', 'fa-lightbulb'],
+        documentation:
+            ['yellow-bg border-[#D97706]', 'fa-clipboard'],
+        'good first issue':
+            ['gray-bg border-[#9CA3AF]', 'fa-goodreads']
     };
 
     if (!labels || labels.length === 0) return '';
     
-    const labelData = labels.map(label => {
+    return labels.map(label => {
         const style = labelStyles[label.toLowerCase()];
 
         if (!style) return '';
 
-        const styleTag = `<p class="${style.classes}">${style.icon} ${label.toUpperCase()}</p>`;
+        const [classes, icon] = style;
 
-        return styleTag;
+        return `
+            <p class="rounded-2xl py-2 ${isModal ? '' : 'md:max-w-[100px]'} ${classes}">
+                <i class="fa-solid ${icon}"></i> ${label.toUpperCase()}
+            </p>
+        `;
     }).join('');
-
-    return labelData;
 }
 
 // display issue card
 const displayIssueCard = (issue) => {
     return `
-        <div id="issue-card" class="card bg-base-100 shadow-lg border-t-4 ${issue.status === 'open' ? 'border-[#00A96E]' : 'border-[#A855F7]'}">
+        <div id="issue-card" class="card bg-base-100 shadow-lg border-t-4 ${
+            issue.status === 'open' ? 'border-[#00A96E]' : 'border-[#A855F7]'
+        }">
             <div class="card-body space-y-3">
                 <div class="issue-criteria flex items-center justify-between">
                     <figure>
@@ -200,12 +191,12 @@ const displayIssueCard = (issue) => {
                 </div>
 
                 <div class="issue-description">
-                    <h3 onclick="displayIssueModal(${issue.id})" class="clickable-title font-semibold text-[14px]">${issue.title}</h3>
+                    <h3 data-id="${issue.id}" class="clickable-title font-semibold text-[14px]">${issue.title}</h3>
                 
                     <p class="gray-color text-[12px] pt-2">${issue.description}</p>
                 </div>
 
-                <div class="bug-help-enhance uppercase text-center flex gap-1 md:text-[8px]">
+                <div class="bug-help-enhance uppercase text-center flex gap-1 md:text-[9px]">
                     ${displayLabels(issue.labels)}
                 </div>
             </div>
@@ -226,16 +217,32 @@ const displayIssueList = (filteredIssues) => {
 
     issueBottomContainer.innerHTML = '';
 
-    if (filteredIssues.length === 0) {
-        issueBottomContainer.innerHTML = `<div class="no-issues p-6 text-center gray-color">
-            <p>No issues available</p>
-        </div>`;
+    // if (filteredIssues.length === 0) {
+    //     issueBottomContainer.innerHTML = `
+    //          <div class="no-issues p-6 text-center gray-color">
+    //             <p>No issues available</p>
+    //         </div>`;
 
-        return;
-    }
+    //     return;
+    // }
 
-    filteredIssues.forEach(issue => {
-        issueBottomContainer.innerHTML += displayIssueCard(issue);
+    // filteredIssues.forEach(issue => {
+    //     issueBottomContainer.innerHTML += displayIssueCard(issue);
+    // });
+
+    issueBottomContainer.innerHTML = filteredIssues.length
+        ? filteredIssues.map(displayIssueCard).join('')
+        : `
+            <div class="no-issues p-6 text-center gray-color">
+                <p>No issues available</p>
+            </div>
+        `;
+
+    issueBottomContainer.querySelectorAll('.clickable-title').forEach(title => {
+        title.addEventListener('click', () => {
+            const issueId = title.dataset.id;
+            displayIssueModal(issueId);
+        });
     });
 };
 
@@ -257,10 +264,11 @@ const loadAllIssue = async() => {
 
     const issueUrl = 'https://phi-lab-server.vercel.app/api/v1/lab/issues';
     const issueRes = await fetch(issueUrl);
-    const issueData = await issueRes.json();
+    const {data: issueData} = await issueRes.json();
+
+    allIssues = issueData;
 
     loadingSpinner(false);
-    allIssues = issueData.data;
 };
 
 // switch between tabs
@@ -268,21 +276,15 @@ const switchTab = () => {
     const trackerBtns = document.querySelectorAll('.issue-tracker-btn');
     // console.log(tabContainerBtn);
 
-    const callTheBtn = (event) => {
-        const button = event.currentTarget;
-
-        activeButton(button);
-
-        const status = button.dataset.id.replace('tab-', '');
-
-        currentStatus = status;
-
-        displayIssues(status);
-        updateIssueCount(status);
-    };
-
     trackerBtns.forEach(btn => {
-        btn.addEventListener('click', callTheBtn);
+        btn.addEventListener('click', () => {
+            activeButton(btn);
+
+            currentStatus = btn.dataset.id.replace('tab-', '');
+
+            displayIssues(currentStatus);
+            updateIssueCount(currentStatus);
+        });
     });
 
     // load all as default tab with active button
@@ -299,12 +301,8 @@ const init = async () => {
     switchTab();
 };
 
-init();
-
 // implement search
-const searchInput = document.querySelector('#issue-search');
-// console.log(searchInput);
-
+// search issue using api
 const searchIssues = async (searchText) => {
     // const filteredIssues = allIssues.filter(issue => {
     //     const filteredStatus =
@@ -318,31 +316,28 @@ const searchIssues = async (searchText) => {
     //     return filteredStatus && filteredSearch;
     // });
 
-    if (!searchText) {
-        displayIssues(currentStatus);
-
-        return;
-    }
+    if (!searchText) return displayIssues(currentStatus);
 
     loadingSpinner(true);
 
     const searchUrl = `https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${searchText}`;
     const searchRes = await fetch(searchUrl);
-    const searchData = await searchRes.json();
+    const {data: searchData} = await searchRes.json();
     // console.log(searchData.data);
 
     loadingSpinner(false);
 
-    displayIssueList(searchData.data);
+    displayIssueList(searchData);
 };
 
-searchInput.addEventListener('input', (event) => {
-    const searchText = event
-        .currentTarget
-        .value
-        .toLowerCase()
-        .trim();
-    // console.log(searchText);
-
-    searchIssues(searchText);
+document.querySelector('#issue-search').addEventListener('input', (event) => {
+    searchIssues(
+        event
+            .currentTarget
+            .value
+            .toLowerCase()
+            .trim()
+    );
 });
+
+init();
